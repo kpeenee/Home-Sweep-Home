20f44c49a2561baa8f314116d216e75635a82767
kpeenee <43548093+kpeenee@users.noreply.github.com>
2023-11-14 20:16:57 +0000

    Project Set Up

---
+++ Home-sweep-home/ClickableComponent.gd
@@ -0,0 +1,7 @@
+extends Area2D
+class_name ClickableComponent
+
+func _input_event(viewport: Viewport, event: InputEvent, shape_idx: int) -> void:
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+			print("I have been pressed")
